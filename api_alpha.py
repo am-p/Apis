e1c5b2bc20255dbae8d8dc78ebe604ed79609115
auxerre <ariel@localhost.localdomain>
2021-02-23 11:47:17 -0300

    Seguimos mejorando

--- api_alpha.py
+++ api_alpha.py
@@ -4,7 +4,7 @@ import requests
 
 def getIntra(function, symbol, interval, size, token):
 
-    url="https://www.alphavantage.co/query"
+    url = "https://www.alphavantage.co/query"
     parametros = {
         "function": function,
         "symbol": symbol,
@@ -13,19 +13,23 @@ def getIntra(function, symbol, interval, size, token):
         "apikey": token,
     }
 
-    r=requests.get(url, params=parametros)
-    data=r.json()["Time Series (" + interval + ")"]
-    dataDF=pd.DataFrame.from_dict(data, orient="index")
+    r = requests.get(url, params=parametros)
+    data = r.json()["Time Series (" + interval + ")"]
+    dataDF = pd.DataFrame.from_dict(data, orient="index")
     return dataDF
 
-datos=getIntra("TIME_SERIES_INTRADAY","GGAL","1min","compact","H5EPS1VK18YSQ6WG")
+
+datos = getIntra("TIME_SERIES_INTRADAY", "VOO",
+                 "1min", "compact", "H5EPS1VK18YSQ6WG")
 print(datos)
 
-TOKEN="H5EPS1VK18YSQ6WG"
+TOKEN = "H5EPS1VK18YSQ6WG"
+
+
 def MACD(symbol, interval, series_type="close", fastperiod=12, slowperiod=26, signalperiod=9):
 
-    function="MACD"
-    url="https://www.alphavantage.co/query"
+    function = "MACD"
+    url = "https://www.alphavantage.co/query"
     parametros = {
         "function": function,
         "symbol": symbol,
@@ -47,5 +51,5 @@ def MACD(symbol, interval, series_type="close", fastperiod=12, slowperiod=26, si
     return df
 
 
-data = MACD("AAPL", "5min")
+data = MACD("VOO", "5min")
 print(data)
--- tiingo01.py
+++ tiingo01.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import requests
 
-ticker="SPY"
-startDate="2021-02-01"
-resampleFreq="1min"
-columns="open,high,low,close,volume"
-token="00244882b421a2c0005d55ebb15e84dc2c03bb71"
+ticker = "SPY"
+startDate = "2021-02-01"
+resampleFreq = "1min"
+#columns = "open,high,low,close,volume"
+token = "00244882b421a2c0005d55ebb15e84dc2c03bb71"
 
-urlBase="https://api.tiingo.com/iex/SPY/prices"
-#url=urlBase+"/<ticker>"+ticker
-url=urlBase+"?&startDate="+startDate
-url+="&resampleFreq"+resampleFreq
-url+="&columns"+columns
-url+="&token"+token
-
-r=requests.get(url)
+urlBase = "https://api.tiingo.com/iex/SPY/prices"
+# url=urlBase+"/<ticker>"+ticker
+url = urlBase+"?&startDate="+startDate
+url += "&resampleFreq="+resampleFreq
+# url+="&columns"+columns
+url += "&token="+token
+print(url)
+r = requests.get(url)
 print(r)
